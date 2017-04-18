60480047bd2383be339e54019693b4aca37575b5
James Hall <greatwolf3d@gmail.com>
2017-04-18 15:04:24 -0700

    Generated nav-header and nav-footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NavFooterComponent } from './nav-footer/nav-footer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [HeaderComponent, FooterComponent]
+  declarations: [HomeComponent, AboutComponent, NavFooterComponent]
 })
 export class CoreModule { }
--- src/app/core/nav-footer/nav-footer.component.spec.ts
+++ src/app/core/nav-footer/nav-footer.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import { NavFooterComponent } from './nav-footer.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+describe('NavFooterComponent', () => {
+  let component: NavFooterComponent;
+  let fixture: ComponentFixture<NavFooterComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ NavFooterComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(NavFooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
