d2a4be1bc591dcf92e28c8454e13f76401e8de5e
James Hall <greatwolf3d@gmail.com>
2017-04-19 20:18:18 -0700

    Added Header and Routes

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import {AboutPageComponent} from './about-page/about-page.component';
+import {ContactPageComponent} from './contact-page/contact-page.component';
+import {PricingPageComponent} from './pricing-page/pricing-page.component';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 const routes: Routes = [
   {
     path:'',
     component: HomePageComponent
+  },
+  {
+    path: 'about',
+    component: AboutPageComponent
+  },
+  {
+    path: 'contact',
+    component: ContactPageComponent
+  },
+  {
+    path: 'pricing',
+    component: PricingPageComponent
   },
   {
     path:'**',
