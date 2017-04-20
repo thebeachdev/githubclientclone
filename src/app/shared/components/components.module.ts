a8822ea9783793e8b9d9ace30b1a21b3191a27bf
James Hall <greatwolf3d@gmail.com>
2017-04-20 14:38:51 -0700

    Trimmed

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 import {LoginFormComponent} from './login-form/login-form.component';
 import {SignupFormComponent} from './signup-form/signup-form.component';
 import {PaymentProcessingFormComponent} from './payment-processing-form/payment-processing-form.component';
-import { NavHeaderComponent } from './nav-header/nav-header.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LoginFormComponent, SignupFormComponent, PaymentProcessingFormComponent, NavHeaderComponent]
+  declarations: [LoginFormComponent, SignupFormComponent, PaymentProcessingFormComponent ]
 })
 export class ComponentsModule { }
