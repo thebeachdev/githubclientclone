6e84d2c7361e9e1c9d685fdfd8b54cb2c710834b
James Hall <greatwolf3d@gmail.com>
2017-04-19 18:21:15 -0700

    Added 404 and shit

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { PricingPageComponent } from './pricing-page/pricing-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CoreRoutingModule
   ],
-  declarations: []
+  declarations: [AboutPageComponent, ContactPageComponent, PricingPageComponent, HomePageComponent, NotFoundPageComponent]
 })
 export class CoreModule { }
---
+++ src/app/core/pricing-page/pricing-page.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PricingPageRoutingModule } from './pricing-page-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    PricingPageRoutingModule
+  ],
+  declarations: []
+})
+export class PricingPageModule { }
