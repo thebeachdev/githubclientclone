429a41b6fb81c1a94a218a1a87e138c0ff8760bf
James Hall <greatwolf3d@gmail.com>
2017-04-28 12:09:10 -0700

    Working on login page css

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
-import {AboutPageComponent} from './about-page/about-page.component';
-import {ContactPageComponent} from './contact-page/contact-page.component';
-import {PricingPageComponent} from './pricing-page/pricing-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { PricingPageComponent } from './pricing-page/pricing-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 const routes: Routes = [
   {
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path: 'pricing',
     component: PricingPageComponent
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent
   },
   {
     path:'**',
