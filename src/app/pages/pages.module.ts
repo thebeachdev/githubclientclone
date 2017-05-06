77a1be213486ad29e9aa04e609043a7c3009f57c
James Hall <greatwolf3d@gmail.com>
2017-05-05 18:19:14 -0700

    updated the nav-header/footer

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,6 +5,8 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { AboutPage } from './about-page/about-page.component';
 import { ContactPage } from './contact-page/contact-page.component';
 import { HomePage } from './home-page/home-page.component';
+import { NavFooterComponent } from './nav-footer/nav-footer.component';
+import { NavHeaderComponent } from './nav-header/nav-header.component';
 import { NotFoundPage } from './not-found-page/not-found-page.component';
 import { LoginPage } from './login-page/login-page.component';
 
@@ -20,9 +22,12 @@ import { LoginPage } from './login-page/login-page.component';
     HomePage,
     NotFoundPage,
     LoginPage,
+    NavFooterComponent,
+    NavHeaderComponent
   ],
   exports: [
-    ComponentsModule
+    NavFooterComponent,
+    NavHeaderComponent
   ],
 })
 export class PagesModule { }
